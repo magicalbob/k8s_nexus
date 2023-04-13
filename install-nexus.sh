067acb34305bfe402e0ad2e703fcd93a4de1bbcc
Ian Ellis <ellisiana@gmail.com>
2023-04-13 16:50:49 +0100

    make it work with kind & k8s

--- install-nexus.sh
+++ install-nexus.sh
@@ -1,3 +1,12 @@
+# Make sure cluster exists if Mac
+if [ $(uname) == "Darwin" ]
+  kind  get clusters | grep "No kind clusters found"
+  if [ $? -eq 0 ]
+  then
+      kind create cluster --config kind-config.yaml
+  fi
+fi
+
 # create renovate namespace, if it doesn't exist
 kubectl get ns nexus 2> /dev/null
 if [ $? -eq 1 ]
@@ -12,12 +21,18 @@ kubectl apply -f nexus.svc.yml
 kubectl apply -f nexus.vmoptions.yml
 
 # sort out persistent volume
-NODE_NAME=$(kubectl get nodes |grep control-plan|cut -d\  -f1)
-envsubst < nexus.persistence.yml.template > nexus.persistence.yml
-kubectl apply -f nexus.persistence.yml
+export NODE_NAME=$(kubectl get nodes |grep control-plan|cut -d\  -f1)
+if [ $(uname) == "Darwin" ]
+then
+  envsubst < nexus.deploy.pv.kind.yml.template > nexus.deploy.pv.kind.yml
+  kubectl apply -f nexus.pv.kind.yml
+else
+  envsubst < nexus.deploy.pv.linux.yml.template > nexus.deploy.pv.linux.yml
+  kubectl apply -f nexus.pv.linux.yml
+fi
 
-# create deployment
-kubectl apply -f nexus.deploy.yml
+# create common deployment
+kubectl apply -f nexus.deploy.common.yml
 
 # check status
 kubectl get all -n nexus

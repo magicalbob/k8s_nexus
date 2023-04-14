1f10d7c1f38df99704a25ceedb513ab52bff583d
Ian Ellis <ellisiana@gmail.com>
2023-04-14 09:04:01 +0100

    templated kind-config.yaml

--- install-nexus.sh
+++ install-nexus.sh
@@ -23,6 +23,7 @@ then
   kind  get clusters 2>&1 | grep "No kind clusters found"
   if [ $? -eq 0 ]
   then
+      envsubst < kind-config.yaml.template > kind-config.yaml
       kind create cluster --config kind-config.yaml
   fi
 fi

8ee3353028bb64a57f4f0b86da60bcc335b853c9
Ian Ellis <ellisiana@gmail.com>
2023-06-27 11:27:15 +0100

    get before create

--- bin/python_walk.py
+++ bin/python_walk.py
@@ -20,16 +20,23 @@ file_types=['proxy','hosted','group']
 for file_type in file_types:
     for repo_type in os.listdir('repo_config/{}'.format(file_type)):
         print('{}/{}'.format(file_type,repo_type))
-        
+
         # modify url for each file_type and repo_type
         url = f'https://{nexus_host}/service/rest/v1/repositories/{repo_type}/{file_type}'
 
         for file in os.listdir('repo_config/{}/{}'.format(file_type,repo_type)):
             with open('repo_config/{}/{}/{}'.format(file_type,repo_type,file)) as file:
                 data = json.load(file)
-            print(json.dumps(data))
-            response = requests.post(url, headers=headers, data=json.dumps(data), auth=(username, password))
-            # print the response
-            print(response.status_code)
-            print(response.text)
+            
+            # Get existing repositories
+            response = requests.get(f'https://{nexus_host}/service/rest/v1/repositories', auth=(username, password))
+            repos = response.json()
+
+            # Check if repository already exists
+            if not any(repo['name'] == data['name'] for repo in repos):
+                print(json.dumps(data))
+                response = requests.post(url, headers=headers, data=json.dumps(data), auth=(username, password))
+                # print the response
+                print(response.status_code)
+                print(response.text)
 

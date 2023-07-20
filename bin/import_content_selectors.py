fefb0d1e945dd4c317c55acc8fb027d0f9c75178
Ian Ellis <ellisiana@gmail.com>
2023-07-20 21:57:17 +0100

    check content selector exists or not before creating

--- bin/import_content_selectors.py
+++ bin/import_content_selectors.py
@@ -14,18 +14,35 @@ def create_content_selectors():
     with open(json_file, 'r') as f:
         selectors = json.load(f)
 
-    # Iterate over the selectors and create them using the Nexus API
+    # Iterate over the selectors and create/update them using the Nexus API
     for selector in selectors:
         endpoint = f"https://{nexus_host}/service/rest/v1/security/content-selectors"
         headers = {
             'Content-Type': 'application/json'
         }
         auth = (username, password)
-        response = requests.post(endpoint, headers=headers, json=selector, auth=auth)
-        if response.status_code >= 201 and response.status_code <= 204:
-            print(f"Content selector '{selector['name']}' created successfully.")
+
+        # Check if the content selector already exists
+        selector_name = selector['name']
+        existing_selector = get_existing_content_selector(endpoint, headers, auth, selector_name)
+        if existing_selector:
+            print(f"Content selector '{selector_name}' already exists.")
         else:
-            print(f"Failed to create content selector '{selector['name']}'. Status Code: {response.status_code}, Error: {response.text}")
+            response = requests.post(endpoint, headers=headers, json=selector, auth=auth)
+            if response.status_code >= 201 and response.status_code <= 204:
+                print(f"Content selector '{selector_name}' created successfully.")
+            else:
+                print(f"Failed to create content selector '{selector_name}'. Status Code: {response.status_code}, Error: {response.text}")
+
+def get_existing_content_selector(endpoint, headers, auth, selector_name):
+    response = requests.get(endpoint, headers=headers, auth=auth)
+    if response.status_code == 200:
+        selectors = response.json()
+        for selector in selectors:
+            if selector['name'] == selector_name:
+                return selector
+    return None
 
 # Usage
 create_content_selectors()
+

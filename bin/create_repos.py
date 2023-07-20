1599b07d8fe6428c92824d72c990b94575d0dd8e
Ian Ellis <ellisiana@gmail.com>
2023-07-21 00:02:07 +0100

    python_walk.py is now create_repos.py and location of repo_config content is specified by a variable

--- bin/create_repos.py
+++ bin/create_repos.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python3
 import os
 import requests
 import json
 
-# get the username and password from environment variables
+# Get the username and password from environment variables
 username = os.environ.get('NEXUS_USERNAME')
 password = os.environ.get('NEXUS_PASSWORD')
 nexus_host = os.environ.get('NEXUS_HOST')
+repo_config = os.environ.get('NEXUS_REPO_CONFIG')
 
-# define url and headers
+# Define URL and headers
 headers = {
     'accept': 'application/json',
     'Content-Type': 'application/json',
@@ -16,18 +16,21 @@ headers = {
     'X-Nexus-UI': 'true',
 }
 
-file_types=['proxy','hosted','group']
+file_types = ['proxy', 'hosted', 'group']
 for file_type in file_types:
-    for repo_type in os.listdir('repo_config/{}'.format(file_type)):
-        print('{}/{}'.format(file_type,repo_type))
+    file_type_path = os.path.join(repo_config, file_type)
+    for repo_type in os.listdir(file_type_path):
+        repo_type_path = os.path.join(file_type_path, repo_type)
+        print('{}/{}'.format(file_type, repo_type))
 
-        # modify url for each file_type and repo_type
+        # Modify URL for each file_type and repo_type
         url = f'https://{nexus_host}/service/rest/v1/repositories/{repo_type}/{file_type}'
 
-        for file in os.listdir('repo_config/{}/{}'.format(file_type,repo_type)):
-            with open('repo_config/{}/{}/{}'.format(file_type,repo_type,file)) as file:
+        for file in os.listdir(repo_type_path):
+            file_path = os.path.join(repo_type_path, file)
+            with open(file_path) as file:
                 data = json.load(file)
-            
+
             # Get existing repositories
             response = requests.get(f'https://{nexus_host}/service/rest/v1/repositories', auth=(username, password))
             repos = response.json()
@@ -36,7 +39,7 @@ for file_type in file_types:
             if not any(repo['name'] == data['name'] for repo in repos):
                 print(json.dumps(data))
                 response = requests.post(url, headers=headers, data=json.dumps(data), auth=(username, password))
-                # print the response
+                # Print the response
                 print(response.status_code)
                 print(response.text)
 

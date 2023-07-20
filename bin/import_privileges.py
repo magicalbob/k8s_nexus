e7adb2b2c6650570e318a9bfa5f52e822935cf3c
Ian Ellis <ellisiana@gmail.com>
2023-07-20 22:09:14 +0100

    just report that privilege already exists

--- bin/import_privileges.py
+++ bin/import_privileges.py
@@ -13,18 +13,24 @@ def create_content_selector_privileges():
     with open(json_file, 'r') as f:
         privileges = json.load(f)
 
-    # Iterate over the privileges and create them using the Nexus API
+    # Iterate over the privileges and create/update them using the Nexus API
     for privilege in privileges:
         endpoint = f"https://{nexus_host}/service/rest/v1/security/privileges/repository-content-selector"
         headers = {
             'Content-Type': 'application/json'
         }
         response = requests.post(endpoint, headers=headers, json=privilege, auth=(username, password))
-        print(response.status_code)
-        if response.status_code >= 201 and response.status_code <= 204:
+        if response.status_code == 201:
             print(f"Privilege '{privilege['name']}' created successfully.")
+        elif response.status_code == 400:
+            error_message = response.json().get('message')
+            if error_message and f"Privilege '{privilege['name']}' already exists" in error_message:
+                print(f"Privilege '{privilege['name']}' already exists.")
+            else:
+                print(f"Failed to create privilege '{privilege['name']}'. Error: {response.text}")
         else:
             print(f"Failed to create privilege '{privilege['name']}'. Error: {response.text}")
 
 # Usage
 create_content_selector_privileges()
+

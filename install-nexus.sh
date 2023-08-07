3f8f192ec1c0ad5b54e4320077200f2bdf0b4aae
Ian Ellis <ellisiana@gmail.com>
2023-08-07 09:08:14 +0000

    got k8s_nexus working with a native k8s cluster (sort of)

--- install-nexus.sh
+++ install-nexus.sh
@@ -1,19 +1,3 @@
-# Make sure cluster exists if Mac
-kind  get clusters 2>&1 | grep "kind-nexus"
-if [ $? -gt 0 ]
-then
-    envsubst < kind-config.yaml.template > kind-config.yaml
-    kind create cluster --config kind-config.yaml --name kind-nexus
-fi
-
-# Make sure create cluster succeeded
-kind  get clusters 2>&1 | grep "kind-nexus"
-if [ $? -gt 0 ]
-then
-    echo "Creation of cluster failed. Aborting."
-    exit 666
-fi
-
 # add metrics
 kubectl apply -f https://dev.ellisbs.co.uk/files/components.yaml
 
@@ -35,8 +19,8 @@ kubectl apply -f nexus.vmoptions.yml
 
 # sort out persistent volume
 export NODE_NAME=$(kubectl get nodes |grep control-plan|cut -d\  -f1)
-envsubst < nexus.deploy.pv.kind.yml.template > nexus.deploy.pv.kind.yml
-kubectl apply -f nexus.deploy.pv.kind.yml
+envsubst < nexus.deploy.pv.linux.yml.template > nexus.deploy.pv.yml
+kubectl apply -f nexus.deploy.pv.yml
 
 # create common deployment
 kubectl apply -f nexus.deploy.common.yml

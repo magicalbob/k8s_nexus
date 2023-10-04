4949118ed542fefce265dc7541486fcd265a36d8
Ian Ellis <ellisiana@gmail.com>
2023-10-04 19:48:15 +0000

    got copy of assets from one nexus to another working

--- bin/copy_assets.py
+++ bin/copy_assets.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import json
+import mimetypes
 
 # Get the old Nexus and new Nexus credentials and hosts from environment variables
 old_nexus_username = os.environ.get('NEXUS_USERNAME')
@@ -13,8 +14,6 @@ new_nexus_host = os.environ.get('NEW_NEXUS_HOST')
 
 # Define URL and headers
 headers = {
-    'accept': 'application/json',
-    'Content-Type': 'application/json',
     'NX-ANTI-CSRF-TOKEN': '0.763232663911438',
     'X-Nexus-UI': 'true',
 }
@@ -26,11 +25,11 @@ repository_settings = response.json()
 # Iterate through repository settings and copy assets
 for repo_settings in repository_settings:
     repo_type = repo_settings.get('type')
-    
+
     # Check if the repository is hosted (exclude groups and proxies)
     if repo_type != 'hosted':
         continue
-    
+
     repo_name = repo_settings['name']
     print(f'Processing repository: {repo_name}')
 
@@ -41,26 +40,23 @@ for repo_settings in repository_settings:
     # Fetch the assets from the old Nexus repository
     response = requests.get(f'{old_nexus_url}', auth=(old_nexus_username, old_nexus_password))
     assets = response.json()
-    
-    # Iterate through assets and upload to the new Nexus repository
-    for asset in assets:
-        if assets['items'] != []:
-            for blob in assets['items']:
-                asset_url = blob['downloadUrl'].replace("http://", "https://")
-                asset_path = blob['path']
-                new_nexus_asset_url = f'{new_nexus_url}'
 
-                with requests.get(asset_url, stream=True, auth=(old_nexus_username, old_nexus_password)) as download_response:
-                    download_response.raise_for_status()
-                    
-                    # Add error handling for the upload process
-                    try:
-                        with requests.post(new_nexus_asset_url, data=download_response.content, auth=(new_nexus_username, new_nexus_password)) as upload_response:
-                            upload_response.raise_for_status()
-                            if upload_response.status_code != 201:
-                                print(f'Error uploading asset {asset_path} to new Nexus repository {repo_name}. Status Code: {upload_response.status_code}')
-                            else:
-                                print(f'Uploaded asset {asset_path} to new Nexus repository {repo_name}')
-                    except requests.exceptions.RequestException as upload_error:
-                        print(f'Error uploading asset {asset_path} to new Nexus repository {repo_name}: {upload_error}')
+    # Iterate through assets and upload to the new Nexus repository
+    for asset in assets.get('items', []):
+        asset_url = asset['downloadUrl'].replace("http://", "https://")
+        asset_path = asset['path']
+
+        # Create a dictionary for the multipart/form-data request
+        files = {'pypi.asset': (asset_path, requests.get(asset_url).content, mimetypes.guess_type(asset_url)[0])}
+
+        # Add error handling for the upload process
+        try:
+            upload_response = requests.post(new_nexus_url, files=files, headers=headers, auth=(new_nexus_username, new_nexus_password))
+            upload_response.raise_for_status()
+            if upload_response.status_code != 204:
+                print(f'Error uploading asset {asset_path} to new Nexus repository {repo_name}. Status Code: {upload_response.status_code}')
+            else:
+                print(f'Uploaded asset {asset_path} to new Nexus repository {repo_name}')
+        except requests.exceptions.RequestException as upload_error:
+            print(f'Error uploading asset {asset_path} to new Nexus repository {repo_name}: {upload_error}')
 

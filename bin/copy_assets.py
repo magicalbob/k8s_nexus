ed67099c6eccabef5553d8eaa46a67de48588d2d
Ian Ellis <ellisiana@gmail.com>
2023-10-06 10:27:11 +0100

    just down load the assets from the source

--- bin/copy_assets.py
+++ bin/copy_assets.py
@@ -3,6 +3,23 @@ import requests
 import json
 import mimetypes
 
+def get_field_name_for_repository(repo_name):
+    # Define a mapping of repository names to field names
+    repository_to_field = {
+        'pypi-hosted': 'pypi.asset',
+        'npm-hosted': 'npm.asset',
+        'maven-hosted': 'maven.asset',
+        # Add more mappings for other repositories as needed
+    }
+    
+    # Check if the repo_name exists in the mapping
+    if repo_name in repository_to_field:
+        return repository_to_field[repo_name]
+    else:
+        # If not found, you can return a default field name or raise an error
+        # For example, return a generic field name:
+        return 'asset'
+
 # Get the old Nexus and new Nexus credentials and hosts from environment variables
 old_nexus_username = os.environ.get('NEXUS_USERNAME')
 old_nexus_password = os.environ.get('NEXUS_PASSWORD')
@@ -11,6 +28,13 @@ old_nexus_host = os.environ.get('NEXUS_HOST')
 new_nexus_username = os.environ.get('NEW_NEXUS_USERNAME')
 new_nexus_password = os.environ.get('NEW_NEXUS_PASSWORD')
 new_nexus_host = os.environ.get('NEW_NEXUS_HOST')
+
+# Define a base directory where assets will be saved locally
+base_directory = 'downloaded_assets'
+
+# Create the base directory if it doesn't exist
+if not os.path.exists(base_directory):
+    os.makedirs(base_directory)
 
 # Define URL and headers
 headers = {
@@ -41,22 +65,26 @@ for repo_settings in repository_settings:
     response = requests.get(f'{old_nexus_url}', auth=(old_nexus_username, old_nexus_password))
     assets = response.json()
 
-    # Iterate through assets and upload to the new Nexus repository
+    # Iterate through assets and download them to the local directory
     for asset in assets.get('items', []):
         asset_url = asset['downloadUrl'].replace("http://", "https://")
         asset_path = asset['path']
 
-        # Create a dictionary for the multipart/form-data request
-        files = {'pypi.asset': (asset_path, requests.get(asset_url).content, mimetypes.guess_type(asset_url)[0])}
+        # Create the full local path for saving the asset
+        local_asset_path = os.path.join(base_directory, asset_path)
 
-        # Add error handling for the upload process
-        try:
-            upload_response = requests.post(new_nexus_url, files=files, headers=headers, auth=(new_nexus_username, new_nexus_password))
-            upload_response.raise_for_status()
-            if upload_response.status_code != 204:
-                print(f'Error uploading asset {asset_path} to new Nexus repository {repo_name}. Status Code: {upload_response.status_code}')
-            else:
-                print(f'Uploaded asset {asset_path} to new Nexus repository {repo_name}')
-        except requests.exceptions.RequestException as upload_error:
-            print(f'Error uploading asset {asset_path} to new Nexus repository {repo_name}: {upload_error}')
+        # Create the directory structure if it doesn't exist
+        local_asset_dir = os.path.dirname(local_asset_path)
+        if not os.path.exists(local_asset_dir):
+            os.makedirs(local_asset_dir)
+
+        # Fetch the asset content
+        asset_content = requests.get(asset_url, auth=(old_nexus_username, old_nexus_password))
+        asset_content.raise_for_status()
+
+        # Save the asset locally
+        with open(local_asset_path, 'wb') as local_file:
+            local_file.write(asset_content.content)
+
+        print(f'Downloaded asset {asset_path} to {local_asset_path}')
 
